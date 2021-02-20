df87a31e3512f47e8107d677dfd19bd2eaca008a
Ivan MSI <arigatory@gmail.com>
2021-02-20 22:43:29 +0300

    Add project files.

---
+++ TextAsyncAnalyzer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace TextAsyncAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var fileName = args[0];
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                var b = fs.ReadByte();
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
